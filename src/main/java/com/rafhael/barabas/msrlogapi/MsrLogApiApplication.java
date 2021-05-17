fe58eeec8aaf2c1cb4e5d6abfb334d3079125aa1
Rafhael de Andrade Barabas <e-barabas@weg.net>
2021-05-17 10:37:22 -0300

    initial

---
+++ src/main/java/com/rafhael/barabas/msrlogapi/MsrLogApiApplication.java
@@ -0,0 +1,13 @@
+package com.rafhael.barabas.msrlogapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsrLogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsrLogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafhael/barabas/msrlogapi/MsrLogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafhael.barabas.msrlogapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsrLogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
